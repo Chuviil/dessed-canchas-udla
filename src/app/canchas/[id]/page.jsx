1d8ab6afe58a9f2ca1ea27e82753472c22a29207
Sebastian Quito <josue.quito@udla.edu.ec>
2025-07-07 23:07:21 -0500

    feat: add ReservationForm component for creating reservations

--- src/app/canchas/[id]/page.jsx
+++ src/app/canchas/[id]/page.jsx
@@ -1,7 +1,10 @@
+import ReservationForm from "./ReservationForm";
+
 export default function DashboardPage() {
     return (
         <div className="flex min-h-screen flex-col justify-center items-center px-4 sm:px-0">
             <h1>Reservas will be here</h1>
+            <ReservationForm />
         </div>
     );
 }
